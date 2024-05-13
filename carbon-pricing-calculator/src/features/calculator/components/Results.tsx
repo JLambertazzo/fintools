ebeaae581815dbf99cad8361dd6ac00bb453c097
Julien <jbertazzolambert@gmail.com>
2024-05-12 22:29:28 -0400

    feat: some basic results

--- carbon-pricing-calculator/src/features/calculator/components/Results.tsx
+++ carbon-pricing-calculator/src/features/calculator/components/Results.tsx
@@ -10,12 +10,34 @@ export function GraphResults(props: GraphResultProps) {
     [props.data],
   );
 
+  const getFirstNegativeYearText = useCallback(() => {
+    const index = props.data.findIndex((element) => element < 0);
+    if (index < 0) {
+      return (
+        <p>Carbon pricing will always be a net economic benefit for you</p>
+      );
+    } else if (index < 1) {
+      return <p>Carbon pricing will never be a net economic benefit for you</p>;
+    } else {
+      return (
+        <p>
+          Carbon pricing is a net economic benefit for you until <br />
+          <span className="ml-2 text-lg font-semibold">May {2023 + index}</span>
+        </p>
+      );
+    }
+  }, [props.data]);
+
   return (
     <div>
       <h2>What the tax means for you:</h2>
       <p>
-        From now until April, 2031, you will {getSum() >= 0 ? "earn" : "lose"} $
-        {getSum()}
+        Carbon pricing will {getSum() >= 0 ? "earn" : "cost"} you <br />
+        <span className="ml-2 text-lg font-semibold">${getSum()}</span>
+      </p>
+      {getFirstNegativeYearText()}
+      <p className="text-xs font-thin">
+        This estimate accounts for both direct and indirect gains/losses
       </p>
     </div>
   );
